523d38a27aab61e7871a57edd039cf17da238975
Alex Kroll <krollus@gmail.com>
2017-01-12 10:34:13 +0300

    module2-solution

---
+++ module2-solution/js/app.js
@@ -0,0 +1,55 @@
+(function(){
+    'use strict';
+    angular.module('ShoppingListCheckOff', [])
+        .controller('toBuyCtl', ToBuyController)
+        .controller('alreadyBoughtCtl', AlreadyBoughtController)
+        .service('ShoppingList', ShoppingListService)
+    
+    ToBuyController.$inject = ['ShoppingList'];
+    function ToBuyController(ShoppingList) {
+        var listToBuy = this;
+        listToBuy.items = ShoppingList.getToBuyList();
+        listToBuy.buyItem = function(idx) {
+            ShoppingList.buyItem(idx);
+        }
+        
+    }
+    
+    AlreadyBoughtController.$inject = ['ShoppingList'];
+    function AlreadyBoughtController(ShoppingList) {
+        var listBought = this;
+        listBought.items = ShoppingList.getBoughtList();
+        
+    }
+
+    function ShoppingListService(){
+        var service = this;
+        var toBuyList = [
+            {
+                name: "Snacks",
+                quantity: 10
+            },
+            {
+                name: "Beer",
+                quantity: 20
+            }
+        ];
+        var boughtList = [];
+
+        service.buyItem = function(idx) {
+            var item = toBuyList[idx];
+            boughtList.push(item);
+            toBuyList.splice(idx, 1);
+        }
+
+        service.getToBuyList = function() {
+            return toBuyList;
+        }
+
+        service.getBoughtList = function() {
+            return boughtList;
+        }
+
+    }
+
+})();
