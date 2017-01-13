8971f3470d76400d6a7ae1b77a1d889e8be3d1c9
Alex Kroll <krollus@gmail.com>
2017-01-13 18:32:50 +0300

    Module3 solution init

---
+++ module3-solution/js/app.js
@@ -0,0 +1,54 @@
+(function(){
+    'use strict';
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('NarrowItDownService', NarrowItDownService)
+        .directive("foundItems", FoundItemsDirective)
+        .constant("ApiBasePath", "https://davids-restaurant.herokuapp.com");
+
+    function FoundItemsDirective() {
+        var ddo = {
+            scope: {
+                items: '<foundItems'  
+            },
+            templateUrl: "foundItems.html",
+            controller: FoundItemsDirectiveController,
+            controllerAs: 'found',
+            bindToController: true
+        }
+        return ddo;
+    };
+
+    function FoundItemsDirectiveController() {
+        var items = this;
+    };
+    
+    NarrowItDownController.$inject = ['NarrowItDownService'];
+    function NarrowItDownController(NarrowItDownService) {
+        var narrowCtl = this;
+        narrowCtl.searchTerm = "ass";
+        narrowCtl.foundItems = Array();
+        narrowCtl.onSearch = function() {
+            NarrowItDownService.getMatchedMenuItems(narrowCtl.searchTerm)
+                .then(function(res) {
+                    narrowCtl.foundItems = res;
+                }).catch(function(error) {console.log("Smth went wrong!", error)});
+        };
+    };
+
+    NarrowItDownService.$inject=['$http', 'ApiBasePath'];
+    function NarrowItDownService($http, ApiBasePath) {
+        var service = this;
+        service.getMatchedMenuItems = function (searchTerm) {
+            return $http({
+                method: "GET",
+                url: (ApiBasePath + "/menu_items.json")
+            }).then(function(result) {
+                var foundItems = result.data.menu_items.filter(function(val) {
+                    return val.description.toLowerCase().indexOf(searchTerm.toLowerCase()) != -1;
+                });
+                return foundItems;
+            });
+        };
+    };
+})();
